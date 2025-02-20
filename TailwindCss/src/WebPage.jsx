482d87c511f796b4461c37acfd22ff89ef993653
tambehimanshu <tambehimanshu18@gmail.com>
2025-02-20 13:42:45 +0530

    photos add to check the respnsiveness

--- TailwindCss/src/WebPage.jsx
+++ TailwindCss/src/WebPage.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 function WebPage() {
   return (
     <>
+    
     <body className='bg-black text-black' >
         {/* navbar goes here */}
-        <nav className='w-full h-14  bg-indigo-200  flex justify-between px-4 items-center md:px-4'>
+        <nav className=' fixed top-0 left-0 w-full h-14  bg-indigo-200  flex justify-between px-4 items-center md:px-4'>
             <div className='text-2xl text-indigo-700 font-bold '>
                 CRICKET
             </div>
@@ -20,9 +21,19 @@ function WebPage() {
             </div>
         </nav>
         {/* navbar ends here */}
+        
+        {/* image section start here */}
+        <div className='w-full'>
+        <header className='w-full h-auto'>
+          <img className='w-full hidden h-screen object-cover md:block' src="https://images.firstpost.com/wp-content/uploads/2013/06/2Champions_Trophy_Indiawin_Reuters.jpg" alt="" />
+          <img className='w-full md:hidden' 
+          src="https://th.bing.com/th/id/OIP.OCW5ZGg9HvdfEgGLEs8jqgHaFk?rs=1&pid=ImgDetMain" alt="" />
+        </header>
+        </div>
+          {/* image section done here */}
 
-        <header></header>
-
+          {/* our Students Section  */}
+          
     </body>
     </>
   )
